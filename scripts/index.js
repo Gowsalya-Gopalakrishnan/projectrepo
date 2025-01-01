2e972da537481cedc684490877c2b3e607b4a585
Gowsalya-Gopalakrishnan <gowsalyagopalakrishnan1515@gmail.com>
2025-01-01 15:13:45 +0530

    all the products linked with header,footer

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
 let a = document.getElementById("a");
 a.addEventListener("click",function(){
     console.log("clicked");
-    window.location.href="signup.html";
+    window.location.href="handbags.html";
 })
