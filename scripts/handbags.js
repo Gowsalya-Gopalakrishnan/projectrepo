39dba7083d44302e9f7037d1abd8a2119397a17c
Gowsalya-Gopalakrishnan <gowsalyagopalakrishnan1515@gmail.com>
2025-01-03 13:02:05 +0530

    products sorted

--- scripts/handbags.js
+++ scripts/handbags.js
@@ -1,22 +1,50 @@
 const products = [
-    { name: 'Handbags', price: 800, category: 'GUCCI' },
-    { name: 'Handbags', price: 1500, category: 'MULBERRY' },
-    { name: 'Handbags', price: 400, category: 'TOM FORD' },
-    // { name: 'Handbags', price: 200, category: 'furniture' }
-    
+    {name:"GUCCI",price:800,description:"GUCCI WOMEN'S PYTHON SOFT STIRRUP CLUTCH BAG",image:'./assets/handbagsgucci1455.png',oldPrice:2400},
+    {name:"MULBERRY",price:1500,description:"MULBERRY MINI IRIS HANDBAG",image:'./assets/handbags mulberry 684.png',oldPrice:1240},
+    {name:"TOM FORD",price:400,description:"TOM FORD WOMENS QUILTED SHOULDER BAG IN BLACK",image:'./assets/handbags tomford 1110.png',oldPrice:2990}
 ];
-operations()
-//sorting by name
-let filteredProducts = [...products]
-function operations(){
-let sortByName = document.getElementById("sortbyname");
-sortByName.addEventListener("change",function(){
-    if(sortByName.value === "atoz"){
-        filteredProducts.sort((a,b)=>a.name.localeCompare(b.name));
 
-    }else if(sortByName.value === "ztoa"){
-        filteredProducts.sort((a,b)=>b.name.localeCompare(a.name));
-    }
-    
-});
+let filteredProducts = [...products];
+
+function renderProducts(productsToRender){
+    const productDetails = document.getElementById("product-details");
+    productDetails.innerHTML='';
+
+    productsToRender.forEach(product =>{
+        const productDiv = document.createElement("div");
+        productDiv.classList.add("product");
+
+        productDiv.innerHTML=`
+        <img src="${product.image}" alt="product-image">
+        <h2>${product.name}</h2>
+        <p>${product.description}</p>
+        <span>$${product.oldPrice.toFixed(2)}</span> $${product.price.toFixed(2)}</p>
+        `;
+
+        productDetails.appendChild(productDiv)
+    });
 }
+function operations(){
+    let sortBySelect = document.getElementById("select");
+    sortBySelect.addEventListener("change",function(){
+        const sortValue = sortBySelect.value;
+        if(sortValue === "atoz"){
+            filteredProducts.sort((a,b)=> a.name.localeCompare(b.name));
+        }
+        else if(sortValue === "ztoa"){
+            filteredProducts.sort((a,b)=> b.name.localeCompare(a.name));
+        }
+        else if(sortValue === "htl"){
+            filteredProducts.sort((a,b)=> b.price - a.price);
+        }
+        else if(sortValue === "lth"){
+            filteredProducts.sort((a,b)=> a.price - b.price);
+        }
+
+        renderProducts(filteredProducts);
+    });
+}
+
+renderProducts(filteredProducts);
+operations();
+
