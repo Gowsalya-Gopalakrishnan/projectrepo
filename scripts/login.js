e8710918be4833fe9cd44f769f784c5f4270df87
Gowsalya G <gowsalyagopalakrishnan1515@gmail.com>
2025-02-26 14:29:40 +0530

    Update login.js

--- scripts/login.js
+++ scripts/login.js
@@ -1,15 +1,15 @@
-let form = document.getElementById("form");
-form.addEventListener("submit",function (){
-    event.preventDefault();
-    let email = form.email.value;
-    let password =form.password.value;
-    console.log(email,password);
-    if(email === email && password === password){
-        alert("Login sucessfull....");
-        window.location.href = "frontpage.html";
-        form.reset();
-    }else{
-        alert("Please check the email and password...");
-    }
+// let form = document.getElementById("form");
+// form.addEventListener("submit",function (){
+//     event.preventDefault();
+//     let email = form.email.value;
+//     let password =form.password.value;
+//     console.log(email,password);
+//     if(email === email && password === password){
+//         alert("Login sucessfull....");
+//         window.location.href = "frontpage.html";
+//         form.reset();
+//     }else{
+//         alert("Please check the email and password...");
+//     }
 
-})
+// })
